d5d63ce6bcf8f6473684e92c5d6b1fbc332fe30f
lgns1027 <jhj041027@gmail.com>
2024-01-16 14:56:10 +0900

    사진 파일 설정중

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,21 +7,21 @@ import type { AppProps } from "next/app";
  * @description SEO를 위해 본인의 정보로 수정해주세요.
  */
 const DEFAULT_SEO = {
-  title: "홍길동 | Front-End Dev",
-  description: "안녕하세요, 프론트엔드 개발자 홍길동입니다.",
+  title: "전지훈 | Front-End Dev",
+  description: "안녕하세요, 프론트엔드 개발자 전지훈입니다.",
   canonical: "https://www.naver.com/",
   openGraph: {
     type: "website",
     locale: "ko_KR",
     url: "https://www.naver.com/",
-    title: "홍길동 | Front-End Dev",
-    site_name: "홍길동 | Front-End Dev",
+    title: "전지훈 | Front-End Dev",
+    site_name: "전지훈 | Front-End Dev",
     images: [
       {
         url: "/share.png",
         width: 285,
         height: 167,
-        alt: "홍길동 | Front-End Dev",
+        alt: "전지훈 | Front-End Dev",
       },
     ],
   },
@@ -34,11 +34,11 @@ const DEFAULT_SEO = {
   additionalMetaTags: [
     {
       name: "application-name",
-      content: "홍길동 | Front-End Dev",
+      content: "전지훈 | Front-End Dev",
     },
     {
       name: "msapplication-tooltip",
-      content: "홍길동 | Front-End Dev",
+      content: "전지훈 | Front-End Dev",
     },
     {
       name: "viewport",
