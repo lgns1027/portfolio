f5b60e37d7f09e84f453a3d7f45f74687edb68cd
lgns1027 <jhj041027@gmail.com>
2024-01-16 11:50:52 +0900

    간단 수정

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -10,7 +10,7 @@ const Information = ({ information }: Pick<DataProps, "information">) => {
         <h1 className="leading-[1.15]">
           {/* 포지션에 맞게 문구를 수정해주세요. 혹은, 본인이 원하는 대로 문구를 바꿔주세요. */}
           안녕하세요,
-          <br /> 프론트엔드 개발자{" "}
+          <br /> 풀스택 개발자{" "}
           <span className="text-PRIMARY font-semibold">{information.name}</span>
           입니다.
         </h1>
